6f16ca92e841b1949b7a9ceb807e537b28ba5ee7
Leandro Ostera <leandro@ostera.io>
2016-06-04 14:18:09 +0200

    Typespec for public api

--- src/pry.erl
+++ src/pry.erl
@@ -1,5 +1,9 @@
 -module(pry).
 
+%%====================================================================
+%% Public API
+%%====================================================================
+
 -export([
           dump/0,
           test/0,
@@ -7,11 +11,42 @@
           dummy/1
          ]).
 
+%%====================================================================
+%% Type Definitions
+%%====================================================================
+
+-type name() :: atom() | [char()] | <<>>.
+-type timestamp() :: integer() | {integer(), integer(), integer()} | unset.
+
+-type value() :: tuple().
+-type key()   :: atom().
+-type info()  :: [ {key(), value() } ].
+
+-type event()  :: #{}.
+-type events() :: [ {timestamp(), event()} ].
+
+-export_type([
+              name/0,
+              timestamp/0,
+              value/0,
+              key/0,
+              info/0,
+              event/0,
+              events/0
+             ]).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+-spec dump() -> events().
 dump() ->
   gen_server:call(pry_server, dump).
 
+-spec test() -> ok.
 test() -> test(10).
 
+-spec test(integer()) -> ok.
 test(N) when N =< 0 -> ok;
 test(N) ->
   erlang:spawn(fun () ->
@@ -19,6 +54,7 @@ test(N) ->
                end),
   test(N-1).
 
+-spec dummy(integer()) -> true.
 dummy(N) ->
   receive
   after N -> true
