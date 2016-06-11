891ffcd7b5244f80f88a7994c7db8b82fc1c6209
Leandro Ostera <leandro@ostera.io>
2016-06-11 10:07:55 +0200

    Closes #6.

--- src/pry_server.erl
+++ src/pry_server.erl
@@ -16,23 +16,24 @@
          terminate/2,
          code_change/3]).
 
+-export([name/0]).
+
 %%====================================================================
 %% API functions
 %%====================================================================
 
+-spec name() -> ?MODULE.
+name() -> ?MODULE.
+
 -spec start_link() -> {'ok', pid()}.
 start_link() ->
-  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+  gen_server:start_link({local, name()}, name(), [], []).
 
 -spec initial_state() -> #{}.
 initial_state() ->
   #{
     table => create_table(),
-    tracer => start_tracer(),
-    trace_specs => [
-                    trace_all_processes(),
-                    trace_all_spawn_calls()
-                   ]
+    tracer => pry_tracer:start()
   }.
 
 
@@ -45,7 +46,7 @@ handle_info(_Info, State) ->
   {noreply, State}.
 
 terminate(_Reason, _State) ->
-  stop_tracer(),
+  pry_tracer:stop(),
   ok.
 
 code_change(_OldVsn, State, _Extra) ->
@@ -61,84 +62,6 @@ table_name() -> pry_events.
 -spec create_table() -> atom().
 create_table() ->
   ets:new(table_name(), [ named_table ]).
-
--spec initial_trace_value() -> atom().
-initial_trace_value() -> initializing.
-
--spec tracer_options() -> { fun(), term() }.
-tracer_options() -> {fun tracer_filter/2, initial_trace_value()}.
-
--spec stop_tracer() -> ok.
-stop_tracer() ->
-  dbg:stop_clear().
-
--spec start_tracer() -> pid().
-start_tracer() ->
-  {ok, TracerPid} = dbg:tracer(process, tracer_options()),
-  TracerPid.
-
-trace_all_processes() -> dbg:p(all,call).
-
-tracer_match_options() -> [{'_',[],[{return_trace}]}].
-
-tracer_match_specs() -> [
-                         {erlang, spawn, '_'},
-                         {erlang, spawn_link, '_'},
-                         {proc_lib, spawn, '_'},
-                         {proc_lib, spawn_link, '_'},
-                         {proc_lib, start, '_'},
-                         {proc_lib, start_link, '_'}
-                        ].
-
-trace_all_spawn_calls() ->
-  [ dbg:tpl( Spec, tracer_match_options() ) || Spec <- tracer_match_specs() ].
-
--spec tracer_filter(pry:trace_result(), ok | term()) -> ok.
-tracer_filter({trace, Parent, return_from, N, {ok, Child}}=_Trace, ok) ->
-  tracer_filter({trace, Parent, return_from, N, Child}, ok);
-tracer_filter({trace, _Parent, return_from, _, Child}=Trace, ok) ->
-  Timestamp = os:timestamp(),
-  ProcessInfo = process_info(Child),
-  case mfa_filter(ProcessInfo) of
-    {ok, _}  ->
-      Event = build_event(Trace, ProcessInfo, Timestamp),
-      track(Event),
-      publish(Event)
-      %% setup link to know when it dies
-      %% and when it dies, save an event as well
-      ;
-    {error, Error} ->
-      io:format("Process was blacklisted - ~p\n\n", [ProcessInfo]),
-      Error
-  end;
-tracer_filter(_, _) -> ok.
-
--spec mfa_filter(pry:process_info()) -> undefined | blacklisted | pry:process_info().
-mfa_filter(ProcessInfo) ->
-  case pry_utils:get_mfa_from_process_info(ProcessInfo) of
-    none   -> {error, no_initial_call};
-    {M,_,_}-> case pry_blacklist:is_blacklisted(M) of
-                true  -> {error, blacklisted};
-                false -> {ok, ProcessInfo}
-              end
-  end.
-
--spec build_event(pry:trace_result(), pry:process_info(), pry:timestamp()) -> pry:event().
-build_event({trace, Parent, return_from, _, Child}, ProcessInfo, Timestamp) ->
- #{
-   timestamp => Timestamp,
-   parent => Parent,
-   self   => Child,
-   mfa    => pry_utils:get_mfa_from_process_info(ProcessInfo),
-   info   => ProcessInfo
-  }.
-
--spec track(pry:event()) -> ok.
-track(Event) ->
-  gen_server:cast(?MODULE, {track, Event}).
-
--spec publish(pry:event()) -> ok.
-publish(_) -> ok.
 
 %%====================================================================
 %% Handler functions
---
+++ src/pry_tracer.erl
@@ -0,0 +1,94 @@
+%%%-------------------------------------------------------------------
+%% @doc tracing module in charge of capturing relevant traces
+%% @end
+%%%-------------------------------------------------------------------
+-module(pry_tracer).
+
+-export([
+         start/0,
+         stop/0
+        ]).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+-spec stop() -> ok.
+stop() -> dbg:stop_clear().
+
+-spec start() -> pid().
+start() ->
+  {ok, TracerPid} = dbg:tracer(process, options()),
+  trace_all_processes(),
+  trace_all_spawn_calls(),
+  TracerPid.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+-spec initial_trace_value() -> atom().
+initial_trace_value() -> initializing.
+
+-spec options() -> { fun(), term() }.
+options() -> {fun filter/2, initial_trace_value()}.
+
+trace_all_processes() -> dbg:p(all,call).
+
+match_options() -> [{'_',[],[{return_trace}]}].
+
+match_specs() -> [
+                  {erlang, spawn, '_'},
+                  {erlang, spawn_link, '_'},
+                  {proc_lib, spawn, '_'},
+                  {proc_lib, spawn_link, '_'},
+                  {proc_lib, start, '_'},
+                  {proc_lib, start_link, '_'}
+                 ].
+
+trace_all_spawn_calls() ->
+  [ dbg:tpl( Spec, match_options() ) || Spec <- match_specs() ].
+
+-spec filter(pry:trace_result(), ok | term()) -> ok.
+filter({trace, Parent, return_from, N, {ok, Child}}=_Trace, ok) ->
+  filter({trace, Parent, return_from, N, Child}, ok);
+filter({trace, _Parent, return_from, _, Child}=Trace, ok) ->
+  Timestamp = os:timestamp(),
+  ProcessInfo = process_info(Child),
+  case mfa_filter(ProcessInfo) of
+    {ok, _}  ->
+      Event = build_event(Trace, ProcessInfo, Timestamp),
+      track(Event)
+      %% setup link to know when it dies
+      %% and when it dies, save an event as well
+      ;
+    {error, Error} ->
+      io:format("Process was blacklisted - ~p\n\n", [ProcessInfo]),
+      Error
+  end;
+filter(_, _) -> ok.
+
+-spec mfa_filter(pry:process_info()) -> undefined | blacklisted | pry:process_info().
+mfa_filter(ProcessInfo) ->
+  case pry_utils:get_mfa_from_process_info(ProcessInfo) of
+    none   -> {error, no_initial_call};
+    {M,_,_}-> case pry_blacklist:is_blacklisted(M) of
+                true  -> {error, blacklisted};
+                false -> {ok, ProcessInfo}
+              end
+  end.
+
+-spec build_event(pry:trace_result(), pry:process_info(), pry:timestamp()) -> pry:event().
+build_event({trace, Parent, return_from, _, Child}, ProcessInfo, Timestamp) ->
+ #{
+   timestamp => Timestamp,
+   parent => Parent,
+   self   => Child,
+   mfa    => pry_utils:get_mfa_from_process_info(ProcessInfo),
+   info   => ProcessInfo
+  }.
+
+-spec track(pry:event()) -> ok.
+track(Event) ->
+  gen_server:cast(pry_server:name(), {track, Event}).
+
