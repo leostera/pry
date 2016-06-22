d62d7f5ca10ce6a0b3a767709835f370e2dfbf53
Leandro Ostera <leandro@ostera.io>
2016-06-22 12:55:37 +0200

    Implement gen_event interface

--- src/pry_publisher.erl
+++ src/pry_publisher.erl
@@ -1,5 +1,40 @@
+%%%-------------------------------------------------------------------
+%% @doc pry top level supervisor.
+%% @end
+%%%-------------------------------------------------------------------
+
 -module(pry_publisher).
 
--compile([export_all]).
+-behaviour(gen_event).
 
+%% gen_server callbacks
+-export([init/1,
+         handle_call/2,
+         handle_event/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
 
+%%====================================================================
+%% API functions
+%%====================================================================
+
+init(_Args) ->
+  %% start web_server here
+  ok.
+
+handle_info(_, State) -> {ok, State}.
+
+handle_call(_, S) -> {ok, ok, S}.
+
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+terminate(_Reason, _State) -> ok.
+
+%%====================================================================
+%% Handler functions
+%%====================================================================
+
+handle_event(_Event, S) ->
+  %JSON = jiffy:parse(Event),
+  {ok, S}.
